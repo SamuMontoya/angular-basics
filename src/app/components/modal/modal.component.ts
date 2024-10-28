08caff75ae0cf0cacc0c4cb0d7ff6a8b853fcb97
SamuMontoya <samu7montoya@gmail.com>
2024-10-28 16:11:35 -0500

    refactor: update modal component to add form functionality

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { InputComponent } from '../input/input.component';
+import { Character } from '../../models/Character';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [MatDialogModule, MatIconModule],
+  imports: [MatDialogModule, MatIconModule, InputComponent, FormsModule],
   templateUrl: './modal.component.html',
 })
-export class ModalComponent {}
+export class ModalComponent {
+  form: Character = {
+    name: '',
+    id: 0,
+    img: '',
+    type: '',
+  };
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
