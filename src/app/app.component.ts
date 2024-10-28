fe1f7e6ad8fe01dfb1d53692ebc54287a50a5a93
SamuMontoya <samu7montoya@gmail.com>
2024-10-28 09:28:33 -0500

    refactor: update app component styles and dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, LayoutComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    CardComponent,
+    LayoutComponent,
+    HeaderComponent,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
