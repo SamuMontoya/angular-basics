a37a1d7a3b255f9154e5c260f5f680ca9846b610
SamuMontoya <samu7montoya@gmail.com>
2024-10-28 08:19:21 -0500

    feat: add background and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightYellow: "#FFEEAE",
+        darkBrown: "#9b3a02",
+        honey: "#cb8d32",
+        semiLightYellow: "#ffde8b",
+        darkYellow: "#f7bf2e",
+      },
+    },
   },
   plugins: [],
 };
