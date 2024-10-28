57c84515b47f3fc13beb1dfd4a37d73456f2bdea
SamuMontoya <samu7montoya@gmail.com>
2024-10-28 08:38:41 -0500

    refactor: update app component structure and layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, LayoutComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+})
+export class LayoutComponent {}
